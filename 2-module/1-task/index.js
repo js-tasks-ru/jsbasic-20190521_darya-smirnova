4178173b4c2269294fe99e1d0bad4837f8ada223
Daria Smirnova <dashechkasmi@gmail.com>
2019-06-07 12:55:14 +0300

    Decision module 1 task 3, module 2, task 1,2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,14 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
-function clone (obj) {
+function clone(obj) {
+    const copy = {};
+    for (let key in obj) {
+        if(obj[key] && typeof obj[key] === 'object'){
+            copy[key] = clone(obj[key]);
+        }else{
+            copy[key] = obj[key];
+        }
+    }
+    return copy;
 }
